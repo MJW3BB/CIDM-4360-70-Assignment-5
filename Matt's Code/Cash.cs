4b56f83a0334d1891ad3db9c7a26d00d0e32ee6f
Matthew Webb <mjwebb2@buffs.wtamu.edu>
2019-12-06 21:19:12 -0600

    Added annotations

--- Matt's Code/Cash.cs
+++ Matt's Code/Cash.cs
@@ -4,25 +4,20 @@ using System.Collections.Generic;
 class Cash : Payment
 {
     public double tenderedCash; //Cash provided by customer, e.g. $200
-    public double change; //Change given to customer
+    public double change; // Change should be returned to customer, e.g. if payment amount is $196, the customer provided $200, then he should get back $4 
 
-    public Cash(double amount, string date, double t) : base(amount, date)
-    {
-        tenderedCash = amount;
-        this.change = amount - tenderedCash; 
+    // Cash CONSTRUCTOR
+    
+    public Cash(double amountCash, string dateCash, double givenCash) : base(amountCash, dateCash){ // Call the base class constructor with the needed parameters
+        tenderedCash = amountCash;
+        this.change = amountCash - givenCash; 
     }
-    public double calcChange(double change)
-    {
+    // calcChange METHOD
+    public double calcChange(double change){ //  updates the change attribute, and returns the change. Change = tenderedCash – payment amount. Note that amount in base class (Payment) is private (not protected and not public)
         change = tenderedCash - amount; 
         return this.change;
     }
-
-    public override bool verify(){
-        Console.WriteLine("Payment verified...");
-        return true;
-    }
-    public override string getPaymentType()
-    {
+    public override string getPaymentType(){ // Overrides the base abstract class method,returns the string “Cash” that represents the payment type of this class. To be used when displaying Payment types, like in getBillInfo(). 
         return "Cash";
     }
 
